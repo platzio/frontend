7d856493c41435812be195b65a756b55d64a814b
Zohar Zilberman <popen2@gmail.com>
2022-04-20 15:17:33 +0300

    Run devServer with https

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,8 @@ const { PLATZ_BACKEND = 'http://127.0.0.1:3000' } = process.env
 
 module.exports = {
   devServer: {
+    https: true,
+    compress: true,
     proxy: {
       '^/api': {
         target: PLATZ_BACKEND,
