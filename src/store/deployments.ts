b49bd27befbc0624035d1eeec7860e09da812950
Zohar Zilberman <popen2@gmail.com>
2022-11-09 13:25:08 +0200

    Fix undefined error in findCollectionDependencies

--- src/store/deployments.ts
+++ src/store/deployments.ts
@@ -1,30 +1,31 @@
-import { Deployment } from './models/deployment'
-import { DbTableOrDeploymentResource } from './chart-ext'
-import { useStore } from './'
+import { Deployment } from "./models/deployment";
+import { DbTableOrDeploymentResource } from "./chart-ext";
+import { useStore } from "./";
 
 export function findCollectionDependencies(
     collection: DbTableOrDeploymentResource,
-    id: string,
+    id: string
 ): Deployment[] {
-    const store = useStore()
-    const result = []
+    const store = useStore();
+    const result = [];
 
     for (const deployment of store!.collections.deployments.all) {
         const chart = store!.collections.helmCharts.getOne(
-            deployment.helm_chart_id)
-        if (!chart.values_ui) {
-            continue
+            deployment.helm_chart_id
+        );
+        if (!chart || !chart.values_ui) {
+            continue;
         }
 
         for (const input of chart.values_ui.inputs) {
             if (input.collection !== collection) {
-                continue
+                continue;
             }
             if (deployment.config[input.id] === id) {
-                result.push(deployment)
+                result.push(deployment);
             }
         }
     }
 
-    return result
+    return result;
 }
