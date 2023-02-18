4626df45e1bca1fd9a70c692155cbc4221e1b52d
Zohar Zilberman <popen2@gmail.com>
2023-02-18 12:53:17 +0200

    ConfigValue: Fix error when input is undefined

--- src/components/values/ConfigValue.vue
+++ src/components/values/ConfigValue.vue
@@ -1,66 +1,67 @@
 <template>
-  <div v-if="shouldDisplay || showEmpty">
-    <ArrayValue
-      v-if="input.type === 'array'"
-      :input="input"
-      :value="value"
-      :allValues="allValues"
-      :envId="envId"
-      :showLabel="showLabel"
-      :showEmpty="showEmpty"
-    />
-    <PlainValue
-      v-else-if="input.type === 'text'"
-      :input="input"
-      :value="value"
-      :showLabel="showLabel"
-      :showEmpty="showEmpty"
-    />
-    <PlainValue
-      v-else-if="input.type === 'number'"
-      :input="input"
-      :value="value"
-      :showLabel="showLabel"
-      :showEmpty="showEmpty"
-    />
-    <CheckboxValue
-      v-else-if="input.type === 'Checkbox'"
-      :input="input"
-      :value="value"
-      :showLabel="showLabel"
-      :showEmpty="showEmpty"
-    />
-    <CollectionValue
-      v-else-if="input.type === 'CollectionSelect'"
-      :input="input"
-      :value="value"
-      :envId="envId"
-      :showLabel="showLabel"
-      :showEmpty="showEmpty"
-    />
-    <RadioValue
-      v-else-if="input.type === 'RadioSelect'"
-      :input="input"
-      :value="value"
-      :showLabel="showLabel"
-      :showEmpty="showEmpty"
-    />
-    <DaysHourValue
-      v-else-if="input.type === 'DaysAndHour'"
-      :input="input"
-      :value="value"
-      :showLabel="showLabel"
-      :showEmpty="showEmpty"
-    />
-    <div v-else>
-      <span class="text-secondary me-2">{{ input.label }}:</span>
-      <span class="text-danger">
-        <FaIcon icon="exclamation-triangle" /> I don't know how to render
-        {{ input.type }}
-        fields
-      </span>
+    <div v-if="input && (shouldDisplay || showEmpty)">
+        <ArrayValue
+            v-if="input.type === 'array'"
+            :input="input"
+            :value="value"
+            :allValues="allValues"
+            :envId="envId"
+            :showLabel="showLabel"
+            :showEmpty="showEmpty"
+        />
+        <PlainValue
+            v-else-if="input.type === 'text'"
+            :input="input"
+            :value="value"
+            :showLabel="showLabel"
+            :showEmpty="showEmpty"
+        />
+        <PlainValue
+            v-else-if="input.type === 'number'"
+            :input="input"
+            :value="value"
+            :showLabel="showLabel"
+            :showEmpty="showEmpty"
+        />
+        <CheckboxValue
+            v-else-if="input.type === 'Checkbox'"
+            :input="input"
+            :value="value"
+            :showLabel="showLabel"
+            :showEmpty="showEmpty"
+        />
+        <CollectionValue
+            v-else-if="input.type === 'CollectionSelect'"
+            :input="input"
+            :value="value"
+            :envId="envId"
+            :showLabel="showLabel"
+            :showEmpty="showEmpty"
+        />
+        <RadioValue
+            v-else-if="input.type === 'RadioSelect'"
+            :input="input"
+            :value="value"
+            :showLabel="showLabel"
+            :showEmpty="showEmpty"
+        />
+        <DaysHourValue
+            v-else-if="input.type === 'DaysAndHour'"
+            :input="input"
+            :value="value"
+            :showLabel="showLabel"
+            :showEmpty="showEmpty"
+        />
+        <div v-else>
+            <span class="text-secondary me-2">{{ input.label }}:</span>
+            <span class="text-danger">
+                <FaIcon icon="exclamation-triangle" /> I don't know how to
+                render
+                {{ input.type }}
+                fields
+            </span>
+        </div>
     </div>
-  </div>
 </template>
 
 <script lang="ts">
@@ -68,41 +69,43 @@ import { computed, defineComponent, PropType } from "vue";
 import { HelmChartUiInput } from "@/store/chart-ext";
 
 export default defineComponent({
-  props: {
-    envId: {
-      type: String,
-      required: true,
+    props: {
+        envId: {
+            type: String,
+            required: true,
+        },
+        input: {
+            type: Object as PropType<HelmChartUiInput>,
+        },
+        value: {
+            required: true,
+        },
+        allValues: {
+            type: Object as PropType<Record<string, any>>,
+            required: true,
+        },
+        showLabel: {
+            type: Boolean,
+            default: true,
+        },
+        showEmpty: {
+            type: Boolean,
+            default: false,
+        },
     },
-    input: {
-      type: Object as PropType<HelmChartUiInput>,
-      required: true,
-    },
-    value: {
-      required: true,
-    },
-    allValues: {
-      type: Object as PropType<Record<string, any>>,
-      required: true,
-    },
-    showLabel: {
-      type: Boolean,
-      default: true,
-    },
-    showEmpty: {
-      type: Boolean,
-      default: false,
-    },
-  },
 
-  setup(props) {
-    const shouldDisplay = computed(
-      () =>
-        !props.input.showIfAll ||
-        props.input.showIfAll.every((fv) => props.allValues[fv.field] === fv.value)
-    );
-    return {
-      shouldDisplay,
-    };
-  },
+    setup(props) {
+        const shouldDisplay = computed(
+            () =>
+                props.input &&
+                (!props.input.showIfAll ||
+                    props.input.showIfAll.every(
+                        (fv) => props.allValues[fv.field] === fv.value
+                    ))
+        );
+        return {
+            shouldDisplay,
+        };
+    },
 });
 </script>
