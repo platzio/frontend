0e0594021a29ac3c53ca34a87aa741c48b70aec7
Zohar Zilberman <popen2@gmail.com>
2023-02-18 12:06:01 +0200

    Fix DeploymentForm to check kind is not undefined before setting filters

--- src/pages/main/env/deployments/DeploymentForm.vue
+++ src/pages/main/env/deployments/DeploymentForm.vue
@@ -1,83 +1,93 @@
 <template>
-  <div class="mb-3 form-floating" v-if="canHaveName">
-    <input
-      type="name"
-      class="form-control"
-      id="name"
-      aria-describedby="nameHelp"
-      v-model="new_data.name"
-      required
-      pattern="^[-a-z0-9]+$"
-      :maxlength="maxNameLength"
-      :disabled="working"
-    />
-    <label for="name" class="form-label"> Deployment Name </label>
-    <div id="nameHelp" class="form-text" v-if="new_data.name">
-      <FaIcon icon="keyboard" class="ms-1" fixed-width />
-      Enter a valid DNS name,
-      {{ maxNameLength - new_data.name.length }} characters left
+    <div class="mb-3 form-floating" v-if="canHaveName">
+        <input
+            type="name"
+            class="form-control"
+            id="name"
+            aria-describedby="nameHelp"
+            v-model="new_data.name"
+            required
+            pattern="^[-a-z0-9]+$"
+            :maxlength="maxNameLength"
+            :disabled="working"
+        />
+        <label for="name" class="form-label"> Deployment Name </label>
+        <div id="nameHelp" class="form-text" v-if="new_data.name">
+            <FaIcon icon="keyboard" class="ms-1" fixed-width />
+            Enter a valid DNS name,
+            {{ maxNameLength - new_data.name.length }} characters left
+        </div>
     </div>
-  </div>
 
-  <div class="mb-3 form-floating">
-    <select
-      name="clusterId"
-      class="form-select"
-      aria-label="Select Cluster"
-      v-model="new_data.cluster_id"
-      required
-      :disabled="working"
-    >
-      <option v-for="cluster in possibleClusters" :key="cluster.id" :value="cluster.id">
-        {{ cluster.region_name }} / {{ cluster.name }}
-      </option>
-    </select>
-    <label for="clusterId" class="form-label"> Cluster </label>
-  </div>
+    <div class="mb-3 form-floating">
+        <select
+            name="clusterId"
+            class="form-select"
+            aria-label="Select Cluster"
+            v-model="new_data.cluster_id"
+            required
+            :disabled="working"
+        >
+            <option
+                v-for="cluster in possibleClusters"
+                :key="cluster.id"
+                :value="cluster.id"
+            >
+                {{ cluster.region_name }} / {{ cluster.name }}
+            </option>
+        </select>
+        <label for="clusterId" class="form-label"> Cluster </label>
+    </div>
 
-  <div class="mb-3 form-floating">
-    <select
-      name="helmChartId"
-      class="form-select"
-      aria-label="Select Version"
-      v-model="new_data.helm_chart_id"
-      required
-      :disabled="working"
-    >
-      <option
-        v-for="chart in filteredCharts"
-        :key="chart.id"
-        :value="chart.id"
-        :disabled="!chart.available"
-      >
-        {{ chart.available ? "" : "[DELETED]" }}
-        {{ chart.image_tag }} (from {{ fromNow(chart.created_at) }})
-      </option>
-    </select>
-    <label for="helmChartId" class="form-label">
-      Version
-      <span v-if="chartsLoading" class="ms-2">
-        <FaIcon icon="circle-notch" fixed-width spin />
-        {{ chartsLoadingPercent }}%
-      </span>
-    </label>
-  </div>
+    <div class="mb-3 form-floating">
+        <select
+            name="helmChartId"
+            class="form-select"
+            aria-label="Select Version"
+            v-model="new_data.helm_chart_id"
+            required
+            :disabled="working"
+        >
+            <option
+                v-for="chart in filteredCharts"
+                :key="chart.id"
+                :value="chart.id"
+                :disabled="!chart.available"
+            >
+                {{ chart.available ? "" : "[DELETED]" }}
+                {{ chart.image_tag }} (from {{ fromNow(chart.created_at) }})
+            </option>
+        </select>
+        <label for="helmChartId" class="form-label">
+            Version
+            <span v-if="chartsLoading" class="ms-2">
+                <FaIcon icon="circle-notch" fixed-width spin />
+                {{ chartsLoadingPercent }}%
+            </span>
+        </label>
+    </div>
 
-  <ConfigInputsForm
-    :envId="envId"
-    :uiSchema="newUiSchema"
-    v-model="new_data.config"
-    :disabled="working"
-  />
+    <ConfigInputsForm
+        :envId="envId"
+        :uiSchema="newUiSchema"
+        v-model="new_data.config"
+        :disabled="working"
+    />
 
-  <div class="small my-1 opacity-50">
-    <FaIcon icon="shield-alt" fixed-width />
-    This field is only visible to deployment owners
-  </div>
-  <div class="mb-3 form-floating" v-show="isOwner">
-    <YamlInput id="valuesOverride" v-model="new_data.values_override" :disabled="working" />
-    <label for="valuesOverride" class="form-label"> Values Override (YAML) </label>
-  </div>
+    <div class="small my-1 opacity-50">
+        <FaIcon icon="shield-alt" fixed-width />
+        This field is only visible to deployment owners
+    </div>
+    <div class="mb-3 form-floating" v-show="isOwner">
+        <YamlInput
+            id="valuesOverride"
+            v-model="new_data.values_override"
+            :disabled="working"
+        />
+        <label for="valuesOverride" class="form-label">
+            Values Override (YAML)
+        </label>
+    </div>
 </template>
 
 <script lang="ts">
@@ -91,120 +101,141 @@ import ConfigInputsForm from "./config/ConfigInputsForm.vue";
 import { isDeploymentOwner } from "@/store/permissions";
 
 export default defineComponent({
-  components: {
-    ConfigInputsForm,
-  },
-
-  props: {
-    envId: {
-      type: String,
-      required: true,
+    components: {
+        ConfigInputsForm,
     },
-    currentData: {
-      type: Object as PropType<Deployment>,
-      required: true,
+
+    props: {
+        envId: {
+            type: String,
+            required: true,
+        },
+        currentData: {
+            type: Object as PropType<Deployment>,
+            required: true,
+        },
+        working: {
+            type: Boolean,
+            default: false,
+        },
     },
-    working: {
-      type: Boolean,
-      default: false,
-    },
-  },
 
-  setup(props) {
-    const store = useStore();
-    const new_data = ref<Partial<Deployment>>({});
+    setup(props) {
+        const store = useStore();
+        const new_data = ref<Partial<Deployment>>({});
 
-    const isOwner = computed(
-      () => new_data.value.kind && isDeploymentOwner(props.envId, new_data.value.kind)
-    );
+        const isOwner = computed(
+            () =>
+                new_data.value.kind &&
+                isDeploymentOwner(props.envId, new_data.value.kind)
+        );
 
-    // Max DNS name length, minus deployment prefix, minus the dash
-    const maxNameLength = computed(() => 62 - (new_data.value.kind || "").length);
+        // Max DNS name length, minus deployment prefix, minus the dash
+        const maxNameLength = computed(
+            () => 62 - (new_data.value.kind || "").length
+        );
 
-    const possibleClusters = computed(() =>
-      store!.collections.k8sClusters.all
-        .filter((cluster) => !cluster.ignore)
-        .filter((cluster) => cluster.env_id == props.envId)
-    );
+        const possibleClusters = computed(() =>
+            store!.collections.k8sClusters.all
+                .filter((cluster) => !cluster.ignore)
+                .filter((cluster) => cluster.env_id == props.envId)
+        );
 
-    watch(
-      () => new_data.value.kind,
-      () =>
-        store!.collections.helmCharts.setFilters({
-          kind: new_data.value.kind,
-        })
-    );
+        watch(
+            () => new_data.value.kind,
+            () => {
+                if (new_data.value.kind) {
+                    store!.collections.helmCharts.setFilters({
+                        kind: new_data.value.kind,
+                    });
+                }
+            }
+        );
 
-    const chartsLoading = computed(() => store!.collections.helmCharts.loading);
-    const chartsLoadingPercent = computed(() => store!.collections.helmCharts.loadingPercent);
+        const chartsLoading = computed(
+            () => store!.collections.helmCharts.loading
+        );
+        const chartsLoadingPercent = computed(
+            () => store!.collections.helmCharts.loadingPercent
+        );
 
-    const filteredCharts = computed(() =>
-      store!.collections.helmCharts.all.filter((chart) => {
-        const registry = store!.collections.helmRegistries.getOne(chart.helm_registry_id);
-        return new_data.value.kind === registry.kind;
-      })
-    );
+        const filteredCharts = computed(() =>
+            store!.collections.helmCharts.all.filter((chart) => {
+                const registry = store!.collections.helmRegistries.getOne(
+                    chart.helm_registry_id
+                );
+                return new_data.value.kind === registry.kind;
+            })
+        );
 
-    const newChart = computed(
-      () =>
-        new_data.value &&
-        new_data.value.helm_chart_id &&
-        store!.collections.helmCharts.getOne(new_data.value.helm_chart_id)
-    );
+        const newChart = computed(
+            () =>
+                new_data.value &&
+                new_data.value.helm_chart_id &&
+                store!.collections.helmCharts.getOne(
+                    new_data.value.helm_chart_id
+                )
+        );
 
-    const canHaveName = computed(() => {
-      if (!newChart.value) {
-        return false;
-      }
-      if (!newChart.value.features) {
-        return true;
-      }
-      const features = chartFeatures(newChart.value);
-      if (!features) {
-        return true;
-      }
-      return features.cardinality == HelmChartCardinality.Many;
-    });
-
-    const newUiSchema = computed(() => newChart.value && newChart.value.values_ui);
-
-    watch(
-      () => props.currentData,
-      (newData) => {
-        new_data.value = cloneDeep(newData);
-      },
-      { deep: true }
-    );
-
-    async function save() {
-      if (props.currentData.id) {
-        const updated = await store!.collections.deployments.updateItem({
-          id: props.currentData.id,
-          data: new_data.value,
+        const canHaveName = computed(() => {
+            if (!newChart.value) {
+                return false;
+            }
+            if (!newChart.value.features) {
+                return true;
+            }
+            const features = chartFeatures(newChart.value);
+            if (!features) {
+                return true;
+            }
+            return features.cardinality == HelmChartCardinality.Many;
         });
-        return updated.id;
-      }
-      const deployment = await store!.collections.deployments.createItem(new_data.value);
-      return deployment.id;
-    }
 
-    function fromNow(ts: string) {
-      return moment(ts).fromNow();
-    }
+        const newUiSchema = computed(
+            () => newChart.value && newChart.value.values_ui
+        );
 
-    return {
-      new_data,
-      possibleClusters,
-      chartsLoading,
-      chartsLoadingPercent,
-      filteredCharts,
-      canHaveName,
-      newUiSchema,
-      isOwner,
-      maxNameLength,
-      save,
-      fromNow,
-    };
-  },
+        watch(
+            () => props.currentData,
+            (newData) => {
+                new_data.value = cloneDeep(newData);
+            },
+            { deep: true }
+        );
+
+        async function save() {
+            if (props.currentData.id) {
+                const updated = await store!.collections.deployments.updateItem(
+                    {
+                        id: props.currentData.id,
+                        data: new_data.value,
+                    }
+                );
+                return updated.id;
+            }
+            const deployment = await store!.collections.deployments.createItem(
+                new_data.value
+            );
+            return deployment.id;
+        }
+
+        function fromNow(ts: string) {
+            return moment(ts).fromNow();
+        }
+
+        return {
+            new_data,
+            possibleClusters,
+            chartsLoading,
+            chartsLoadingPercent,
+            filteredCharts,
+            canHaveName,
+            newUiSchema,
+            isOwner,
+            maxNameLength,
+            save,
+            fromNow,
+        };
+    },
 });
 </script>
