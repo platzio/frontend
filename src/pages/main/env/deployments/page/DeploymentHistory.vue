5cdadd748ea45cdaf4fae6ec8714532d4fa53724
Zohar Zilberman <popen2@gmail.com>
2023-02-18 12:32:39 +0200

    DeploymentHistory: Show items after both tasks and charts have loaded

--- src/pages/main/env/deployments/page/DeploymentHistory.vue
+++ src/pages/main/env/deployments/page/DeploymentHistory.vue
@@ -1,22 +1,23 @@
 <template>
-  <div>
-    <div v-if="tasksLoading" class="my-3 text-center text-muted">
-      <FaIcon icon="circle-notch" spin fixed-width />
-      Loading tasks ({{ tasksLoadingPercent }}%)
-    </div>
-    <div v-if="chartsLoading" class="my-3 text-center text-muted">
-      <FaIcon icon="circle-notch" spin fixed-width />
-      Loading charts ({{ chartsLoadingPercent }}%)
-    </div>
+    <div>
+        <div v-if="tasksLoading" class="my-3 text-center text-muted">
+            <FaIcon icon="circle-notch" spin fixed-width />
+            Loading tasks ({{ tasksLoadingPercent }}%)
+        </div>
 
-    <PlatzCollection v-else :items="tasks" flush>
-      <template #item="scope">
-        <PlatzCollectionItem>
-          <DeploymentTask :task="scope.item" :envId="envId" />
-        </PlatzCollectionItem>
-      </template>
-    </PlatzCollection>
-  </div>
+        <div v-else-if="chartsLoading" class="my-3 text-center text-muted">
+            <FaIcon icon="circle-notch" spin fixed-width />
+            Loading charts ({{ chartsLoadingPercent }}%)
+        </div>
+
+        <PlatzCollection v-else :items="tasks" flush>
+            <template #item="scope">
+                <PlatzCollectionItem>
+                    <DeploymentTask :task="scope.item" :envId="envId" />
+                </PlatzCollectionItem>
+            </template>
+        </PlatzCollection>
+    </div>
 </template>
 
 <script lang="ts">
@@ -27,85 +28,99 @@ import { DeploymentTaskStatus } from "@/store/models/deployment-task";
 import DeploymentTask from "../tasks/DeploymentTask.vue";
 
 export default defineComponent({
-  props: {
-    envId: {
-      type: String,
-      required: true,
+    props: {
+        envId: {
+            type: String,
+            required: true,
+        },
+        deployment: {
+            type: Object as PropType<Deployment>,
+            required: true,
+        },
     },
-    deployment: {
-      type: Object as PropType<Deployment>,
-      required: true,
+
+    components: {
+        DeploymentTask,
     },
-  },
 
-  components: {
-    DeploymentTask,
-  },
+    setup(props) {
+        const store = useStore();
 
-  setup(props) {
-    const store = useStore();
+        onMounted(() => {
+            store!.collections.deploymentTasks.setFilters({
+                deployment_id: props.deployment.id,
+            });
+            store!.collections.helmCharts.setFilters({
+                kind: props.deployment.kind,
+            });
+        });
 
-    onMounted(() => {
-      store!.collections.deploymentTasks.setFilters({
-        deployment_id: props.deployment.id,
-      });
-      store!.collections.helmCharts.setFilters({
-        kind: props.deployment.kind,
-      });
-    });
+        const tasksLoading = computed(
+            () => store!.collections.deploymentTasks.loading
+        );
+        const tasksLoadingPercent = computed(
+            () => store!.collections.deploymentTasks.loadingPercent
+        );
+        const chartsLoading = computed(
+            () => store!.collections.helmCharts.loading
+        );
+        const chartsLoadingPercent = computed(
+            () => store!.collections.helmCharts.loadingPercent
+        );
 
-    const tasksLoading = computed(() => store!.collections.deploymentTasks.loading);
-    const tasksLoadingPercent = computed(() => store!.collections.deploymentTasks.loadingPercent);
-    const chartsLoading = computed(() => store!.collections.helmCharts.loading);
-    const chartsLoadingPercent = computed(() => store!.collections.helmCharts.loadingPercent);
+        const tasks = computed(() =>
+            store!.collections.deploymentTasks.all.filter(
+                (task) => task.deployment_id == props.deployment.id
+            )
+        );
 
-    const tasks = computed(() =>
-      store!.collections.deploymentTasks.all.filter(
-        (task) => task.deployment_id == props.deployment.id
-      )
-    );
+        const numPending = computed(() =>
+            tasksLoading.value
+                ? 0
+                : tasks.value.filter(
+                      (task) => task.status == DeploymentTaskStatus.Pending
+                  ).length
+        );
 
-    const numPending = computed(() =>
-      tasksLoading.value
-        ? 0
-        : tasks.value.filter((task) => task.status == DeploymentTaskStatus.Pending).length
-    );
+        const numRunning = computed(() =>
+            tasksLoading.value
+                ? 0
+                : tasks.value.filter(
+                      (task) => task.status == DeploymentTaskStatus.Started
+                  ).length
+        );
 
-    const numRunning = computed(() =>
-      tasksLoading.value
-        ? 0
-        : tasks.value.filter((task) => task.status == DeploymentTaskStatus.Started).length
-    );
+        const numFinished = computed(() =>
+            tasksLoading.value
+                ? 0
+                : tasks.value.length - numPending.value - numRunning.value
+        );
 
-    const numFinished = computed(() =>
-      tasksLoading.value ? 0 : tasks.value.length - numPending.value - numRunning.value
-    );
+        const summary = computed(() => {
+            if (tasksLoading.value) {
+                return "";
+            }
+            let result = `${numFinished.value}`;
+            if (numRunning.value) {
+                result = `${result} + ${numRunning.value} running`;
+            }
+            if (numPending.value) {
+                result = `${result} + ${numPending.value} pending`;
+            }
+            return result;
+        });
 
-    const summary = computed(() => {
-      if (tasksLoading.value) {
-        return "";
-      }
-      let result = `${numFinished.value}`;
-      if (numRunning.value) {
-        result = `${result} + ${numRunning.value} running`;
-      }
-      if (numPending.value) {
-        result = `${result} + ${numPending.value} pending`;
-      }
-      return result;
-    });
-
-    return {
-      tasksLoading,
-      tasksLoadingPercent,
-      chartsLoading,
-      chartsLoadingPercent,
-      tasks,
-      numPending,
-      numRunning,
-      numFinished,
-      summary,
-    };
-  },
+        return {
+            tasksLoading,
+            tasksLoadingPercent,
+            chartsLoading,
+            chartsLoadingPercent,
+            tasks,
+            numPending,
+            numRunning,
+            numFinished,
+            summary,
+        };
+    },
 });
 </script>
