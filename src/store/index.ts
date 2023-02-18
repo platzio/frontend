2f5403aa71e2a102ab06a0637cc6959365d3cdd8
Zohar Zilberman <popen2@gmail.com>
2023-02-18 12:54:51 +0200

    store: Avoid collection loading same filters more than once

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,34 +1,46 @@
-import { computed, inject, watch } from 'vue'
-import { createAuth } from './auth'
-import { createCollections } from './collections'
-import { startWsUpdates } from './ws'
+import { computed, inject, watch } from "vue";
+import { createAuth } from "./auth";
+import { createCollections } from "./collections";
+import { startWsUpdates } from "./ws";
 
 export function createStore() {
-    const auth = createAuth()
-    const { collections, tableNameToCollection } = createCollections()
-    const wsUpdates = startWsUpdates({ collections, tableNameToCollection })
+    const auth = createAuth();
+    const { collections, tableNameToCollection } = createCollections();
+    const wsUpdates = startWsUpdates({ collections, tableNameToCollection });
 
-    watch(() => auth.ready, (isReady: boolean) => {
-        if (isReady) {
-            wsUpdates.start()
-            for (const collection of Object.values(collections)) {
-                collection.readAllItems()
-            }
-        } else {
-            wsUpdates.stop()
-            for (const collection of Object.values(collections)) {
-                collection.reset()
+    watch(
+        () => auth.ready,
+        (isReady: boolean) => {
+            if (isReady) {
+                wsUpdates.start();
+                for (const collection of Object.values(collections)) {
+                    collection.init();
+                }
+            } else {
+                wsUpdates.stop();
+                for (const collection of Object.values(collections)) {
+                    collection.reset();
+                }
             }
         }
-    })
+    );
 
-    const modules = [auth, wsUpdates, ...Object.values(collections)]
+    const modules = [auth, wsUpdates, ...Object.values(collections)];
 
-    const ready = computed(() => modules.every(mod => mod.ready))
-    const loadingProgress = computed(() => modules.reduce((sum, mod) => sum + (mod.ready ? 100 : 0), 0) / modules.length)
-    const loadingStatus = computed(() => modules.reduce((status, mod) => mod.status || status, null as string | null))
+    const ready = computed(() => modules.every((mod) => mod.ready));
+    const loadingProgress = computed(
+        () =>
+            modules.reduce((sum, mod) => sum + (mod.ready ? 100 : 0), 0) /
+            modules.length
+    );
+    const loadingStatus = computed(() =>
+        modules.reduce(
+            (status, mod) => mod.status || status,
+            null as string | null
+        )
+    );
 
-    auth.start()
+    auth.start();
 
     return {
         auth,
@@ -38,8 +50,9 @@ export function createStore() {
         ready,
         loadingProgress,
         loadingStatus,
-    }
+    };
 }
 
-export const storeSymbol = Symbol('store')
-export const useStore = () => inject<ReturnType<typeof createStore>>(storeSymbol)
+export const storeSymbol = Symbol("store");
+export const useStore = () =>
+    inject<ReturnType<typeof createStore>>(storeSymbol);
