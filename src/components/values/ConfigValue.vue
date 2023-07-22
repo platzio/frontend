403c385f78737f78a77facd2d221e04290cbfbd8
Zohar Zilberman <28088+popen2@users.noreply.github.com>
2023-07-22 11:06:12 +0300

    Update src/components/values/ConfigValue.vue

--- src/components/values/ConfigValue.vue
+++ src/components/values/ConfigValue.vue
@@ -117,8 +117,7 @@ export default defineComponent({
                     return true
                 }
             }
-
-            );
+        );
         return {
             shouldDisplay,
         };
